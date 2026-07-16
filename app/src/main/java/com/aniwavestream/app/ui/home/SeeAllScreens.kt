b71f990ce1fd17e120815e147d4a618ec7c40218
AniwaveStream Dev <dev@aniwavestream.local>
2026-07-16 16:40:45 +0000

    fix: add weight import for type-nav row

--- app/src/main/java/com/aniwavestream/app/ui/home/SeeAllScreens.kt
+++ app/src/main/java/com/aniwavestream/app/ui/home/SeeAllScreens.kt
@@ -2,6 +2,7 @@ package com.aniwavestream.app.ui.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
